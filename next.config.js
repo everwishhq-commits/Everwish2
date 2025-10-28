cf973576eee1ceb535168a44978ee1901f562e15
everwishhq-commits <everwishhq@gmail.com>
2025-10-27 22:46:32 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,15 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true, // ayuda a detectar errores en desarrollo
-  swcMinify: true,       // optimiza el rendimiento del build
+  reactStrictMode: true,
+  swcMinify: true,
   images: {
     domains: ["everwish.store", "everwishs-projects.vercel.app"],
   },
   eslint: {
-    ignoreDuringBuilds: true, // evita que falle el build si hay warnings
-  },
-  experimental: {
-    appDir: true, // asegura compatibilidad total con la carpeta /app
+    ignoreDuringBuilds: true,
   },
 };
 
